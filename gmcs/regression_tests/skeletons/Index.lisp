64ab99db6462c444e717118e71662cddc68bfbbb
olzama <olzama@users.noreply.github.com>
2017-12-20 23:57:50 +0000

    readded test

--- gmcs/regression_tests/skeletons/Index.lisp
+++ gmcs/regression_tests/skeletons/Index.lisp
@@ -22,6 +22,7 @@
 (
 ((:path . "tiniest") (:content . "tiniest: A very basic grammar just to get the regression tests started.  SOV word order, no frills."))
  ;;; new-regression-test-here
+((:path . "ccomp-extra-and-same") (:content . "ccomp-extra-and-same: Clausal complements: SOV, extraposed and normally positioned complement."))
 ((:path . "ccomp-v2-vfinal-aux") (:content . "ccomp-v2-vfinal-aux: Clausal comps: V2 in matrix, Vfinal in embedded, auxiliaries cluster with the verb."))
 ((:path . "clausalcomp-extra-and-same") (:content . "clausalcomp-extra-and-same: Clausal-complement: SOV, extraposed and nouny position complement."))
 ((:path . "clcomp-sov-extra") (:content . "clcomp-sov-extra: Clausal comps: SOV, extraposed complement."))
