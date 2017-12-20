2718b50d4aea8984789e108d3be87bc7a697f772
olzama <olzama@users.noreply.github.com>
2017-12-20 20:03:12 +0000

    Hopefully fixed a bug in customize_test_sentences() where a space between the asterisk and the first word was causing problems if the test suite was later used with tsdb

--- gmcs/customize.py
+++ gmcs/customize.py
@@ -195,7 +195,7 @@ def customize_test_sentences(grammar_path):
                     # # Shouldn't the start be printed in test_sentences
                     # if the sentence is ungrammatical? See choices.py uprev convert_23_to_24() though.
                     if sentence['star'] == 'on':
-                        ts.write('* ' + sentence.get('orth','') + '\n')
+                        ts.write('*' + sentence.get('orth','') + '\n')
                     else:
                         ts.write(sentence.get('orth','') + '\n')
                 s.write(')))\n')
