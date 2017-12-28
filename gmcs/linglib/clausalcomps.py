d77e87ca5db5ae7a4063be0217aaab0a7c0f3ffe
olzama <olzama@users.noreply.github.com>
2017-12-28 21:55:43 +0000

    Constrain complementizers for EXTRA if EXTRA is used

--- gmcs/linglib/clausalcomps.py
+++ gmcs/linglib/clausalcomps.py
@@ -303,7 +303,10 @@ def constrain_lex_items(head,ch,cs,comptype, init_value, default_init_value,myla
     if comptype and nominalized_comps(ch) and not is_nominalized_complement(cs):
         mylang.add(comptype + ':= [ SYNSEM.LOCAL.CAT.VAL.COMPS < [ LOCAL.CAT.HEAD.NMZ - ] > ].',merge=True)
     if comptype and extra_needed(ch,mylang):
-        pass
+        if cs[CLAUSE_POS_EXTRA] and not cs[CLAUSE_POS_SAME]:
+            mylang.add(comptype + ':= [ SYNSEM.LOCAL.CAT.VAL.COMPS < [ LOCAL.CAT.HEAD.EXTRA + ] > ].',merge=True)
+        elif cs[CLAUSE_POS_SAME] and not cs[CLAUSE_POS_EXTRA]:
+            mylang.add(comptype + ':= [ SYNSEM.LOCAL.CAT.VAL.COMPS < [ LOCAL.CAT.HEAD.EXTRA - ] > ].',merge=True)
 
 
 def constrain_transitive_verb(head,cs):
@@ -431,7 +434,6 @@ def extra_needed(ch,mylang):
            and len([cs for cs in ch[COMPS] if cs[CLAUSE_POS_EXTRA]]) > 0
     if res:
         mylang.add('head :+ [ EXTRA bool ].', section='addenda')
-        #if len([cs for cs in ch[COMPS] if cs[CLAUSE_POS_SAME]]) == 0:
         mylang.add('transitive-verb-lex := [ SYNSEM.LOCAL.CAT.VAL.COMPS < [ LOCAL.CAT.HEAD.EXTRA - ] > ].'
                        ,merge=True)
     return res
