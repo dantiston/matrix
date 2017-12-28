57cbbc058a0f0a22d9bd26bdd66c4c3ee4ecd1c4
olzama <olzama@users.noreply.github.com>
2017-12-28 18:23:32 +0000

    work in progress

--- gmcs/linglib/clausalcomps.py
+++ gmcs/linglib/clausalcomps.py
@@ -300,12 +300,13 @@ def constrain_lex_items(head,ch,cs,comptype, init_value, default_init_value,myla
             constrain_lexitem_for_feature(clausalverb,init_path, 'INIT', init_value,mylang)
         mylang.add('transitive-verb-lex := [ ' + init_path + '.INIT ' + default_init_value + ' ].'
                    , merge=True)
-    elif head == 'comp':
+    #elif head == 'comp':
+    else:
         if (cs[COMP_POS_BEFORE] and not cs[COMP_POS_AFTER] and ch.get(constants.WORD_ORDER) in OV_ORDERS) \
                 or (cs[COMP_POS_AFTER] and ch.get(constants.WORD_ORDER) in VO_ORDERS):
             mylang.add(comptype + ':= [ ' + init_path + '.INIT ' + init_value + ' ].',merge=True)
-        else:
-            mylang.add(comptype + ':= [ ' + init_path + '.INIT ' + default_init_value + ' ].',merge=True)
+        #else:
+        #    mylang.add(comptype + ':= [ ' + init_path + '.INIT ' + default_init_value + ' ].',merge=True)
 
 
 def constrain_lexitem_for_feature(typename, feature_path, feature_name, feature_value,mylang):
