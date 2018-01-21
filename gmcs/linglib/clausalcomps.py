a05b9da2b551b50edc2fba4fee2e200775897d8e
olzama <olzama@users.noreply.github.com>
2018-01-21 18:06:36 +0000

    Try to simplify complementizer_HCR needed

--- gmcs/linglib/clausalcomps.py
+++ gmcs/linglib/clausalcomps.py
@@ -296,12 +296,12 @@ def complementizer_comp_head_needed(wo,cs):
     flex_cl = cs[EXTRA] and cs[SAME]
     #Complementizer position is flexible
     flex_comp = cs[BEF] and cs[AFT]
-    #if wo in ['vos'] and flex_cl and flex_comp:
-    #  return False
+    if wo in ['vos'] and flex_cl and flex_comp:
+      return False
     #if wo  == 'v-initial' and cs[EXTRA] and not cs[AFT]:
     #    return False
-    #if wo == 'vos' and cs[EXTRA] and cs[AFT]:
-    #    return True
+    if wo == 'vos' and cs[EXTRA] and cs[AFT]:
+        return True
     if wo == 'v-final' and cs[EXTRA] and not cs[SAME] and cs[AFT]:
         return True
     if not wo == 'v-final' and cs[SAME] and cs[COMP] and (cs[EXTRA] or cs[AFT]):
