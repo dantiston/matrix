1e3a1281b38cb6dd15a5f8edefbea4e9607f2016
olzama <olzama@users.noreply.github.com>
2018-01-27 02:08:44 +0000

    merged with ekayen

--- gmcs/validate.py
+++ gmcs/validate.py
@@ -1449,20 +1449,20 @@ def validate_arg_opt(ch, vr):
 
 ######################################################################
 # Validation of clausal modifiers
-def validate_clausalmods(ch, vr):
-    """Check to see if the user completed the necessary portions of the
-       Clausal Modifiers page and check for unsupported combinations of choices"""
-    for cms in ch.get('cms'):
-        # First check the choices that are required for all clausal mod strategies
-        if not cms.get('position'):
-            mess = 'You must select a position for the clausal modifier.'
-            vr.err(cms.full_key + '_position', mess)
-        if not cms.get('modifier-attach'):
-            mess = 'You must select VP or S attachment for the clausal modifier.'
-            vr.err(cms.full_key + '_modifier-attach', mess)
-        if not cms.get('subordinator'):
-            mess = 'You must select a subordinator type.'
-            vr.err(cms.full_key + '_subordinator', mess)
+# def validate_clausalmods(ch, vr):
+#     """Check to see if the user completed the necessary portions of the
+#        Clausal Modifiers page and check for unsupported combinations of choices"""
+#     for cms in ch.get('cms'):
+#         # First check the choices that are required for all clausal mod strategies
+#         if not cms.get('position'):
+#             mess = 'You must select a position for the clausal modifier.'
+#             vr.err(cms.full_key + '_position', mess)
+#         if not cms.get('modifier-attach'):
+#             mess = 'You must select VP or S attachment for the clausal modifier.'
+#             vr.err(cms.full_key + '_modifier-attach', mess)
+#         if not cms.get('subordinator'):
+#             mess = 'You must select a subordinator type.'
+#             vr.err(cms.full_key + '_subordinator', mess)
 
 ######################################################################
 # Validation of clausal modifiers
